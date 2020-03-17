b574e76b54c24f1734bbd8a99d5cd59a51446932
syedgufran1997 <syedgufran1997@gmail.com>
2020-03-17 13:44:33 +0530

    practice

---
+++ index.js
@@ -0,0 +1,136 @@
+/*var profession = "Engineering";
+
+var reverse = "";
+for (var i = 0; i < profession.length; i++) {
+  reverse = profession.charAt(i) + reverse;
+}
+console.log(reverse);
+
+// control loops(if statement)
+
+
+var ihavemobile = true;
+if (ihavemobile) {
+  console.log("this expression is true");
+  document.write("i have redmi4");
+}
+
+
+var mynum = 8;
+
+if (mynum == 10) {
+  console.log("my num equal to 10");
+  document.write("my num equal to 10");
+} else if (mynum < 10) {
+  console.log("my num is less than 10");
+}
+
+var youlikemeat = false;
+
+if (youlikemeat) {
+  console.log("here is meat menu");
+} else {
+  console.log("we have veg menu");
+}
+
+
+var myage = 9;
+if (myage > 30) {
+  console.log("you are over 30!");
+} else if (myage > 20) {
+  console.log("you are over 20!");
+} else if (myage > 10) {
+  console.log("you are over 10!");
+} else {
+  console.log("you are not even 10!");
+}
+
+var myage = 15;
+
+if (myage >= 18 || myage <= 30) {
+  console.log("you are eligible!");
+} else {
+  console.log("You are not eligible!");
+}
+
+var age = 8;
+while (age < 10) {
+  console.log("your age is less than 10");
+  age++;
+}
+document.write("you are now over 10");
+
+var links = document.getElementsByTagName("a");
+
+for (i = 1; i <= links.length; i++) {
+  console.log("This is link number" + i);
+}
+document.write("All links are looped");
+
+for (i = 0; i < 10; i++) {
+  if (i === 2 || i === 5) {
+    continue;
+  }
+  console.log(i);
+
+  if (i === 8) {
+    break;
+  }
+}
+console.log("break loop");
+
+var links = document.getElementsByTagName("a");
+
+for (i = 0; i < links.length; i++) {
+  links[i].className = "link-" + i;
+}
+
+function getaverage(a, b) {
+  var average = (a + b) / 2;
+  console.log(average);
+  return average;
+}
+var myResult = getaverage(7, 11);
+
+function logresult() {
+  console.log("the average is  " + myResult + " inside the function");
+}
+logresult();
+
+var a = "5";
+var b = 5;
+
+if (!isNaN(a)) {
+  console.log("answer is " + a / b);
+}
+else {
+  console.log("answer is " + a / b);
+}
+
+var mystring = 'i\'m a "fun" ninja string';
+
+if (mystring.indexOf("ninja") === -1) {
+  console.log("the word ninja is not in string");
+} else {
+  console.log("the word is at " + mystring.indexOf("ninja"));
+}
+
+var mycar = new Object();
+mycar.driver = "tony";
+mycar.speed = 70;
+mycar.drive = function() {
+  console.log("driving");
+};
+
+mycar.drive();
+
+var mycar2 = {
+  maxspeed: 70,
+  driver: "tony",
+  drive: function() {
+    console.log("now driving");
+  }
+};
+console.log(mycar2.driver);
+mycar2.drive();
+*/
